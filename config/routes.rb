d12f74f9e461da43f9032cc570885beaace008aa
Kaare Hoff Skovgaard <kaare@kaareskovgaard.net>
2013-04-28 22:40:04 +0200

    Minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -47,19 +47,7 @@ Contractor::Application.routes.draw do
 	#   end
 
 
-	resources :certificates do
-		member do
-			get 'show'
-			get 'new'
-			get 'crl'
-			post 'create'
-			delete 'destroy'
-		end
-
-		collection do
-			get 'index'
-		end
-	end
+	resources :certificates
 
 	# You can have the root of your site routed with "root"
 	# just remember to delete public/index.html.
